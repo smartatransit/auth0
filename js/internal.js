e719a47110b3ee4beacee6174a69a41f7af4c11a
Xander Flood <xander.t.flood@gmail.com>
2020-03-17 22:48:51 -0400

    stray line

--- js/internal.js
+++ js/internal.js
@@ -1,19 +1,18 @@
 module.exports = function(client, scope, audience, context, cb) {
-  var access_token = {};
-  scope = ["a","b","c"];
-  access_token.scope = scope; // do not remove this line
+	var access_token = {};
+	access_token.scope = scope; // do not remove this line
 
-  const namespace = 'https://ataper.net/';
-  console.log(scope);
-  if (scope) {
-    if (scope.indexOf("be:anonymous") > -1) {
-      access_token[namespace + "role"] = "anonymous";
-    } else if (scope.indexOf("be:internal") > -1) {
-      access_token[namespace + "role"] = "internal";
-    } else if (scope.indexOf("be:user") > -1) {
-      access_token[namespace + "role"] = "user";
-    }
-  }
+	const namespace = 'https://ataper.net/';
+	console.log(scope);
+	if (scope) {
+		if (scope.indexOf("be:anonymous") > -1) {
+			access_token[namespace + "role"] = "anonymous";
+		} else if (scope.indexOf("be:internal") > -1) {
+			access_token[namespace + "role"] = "internal";
+		} else if (scope.indexOf("be:user") > -1) {
+			access_token[namespace + "role"] = "user";
+		}
+	}
 
-  cb(null, access_token);
+	cb(null, access_token);
 };
