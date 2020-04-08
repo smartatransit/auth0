b03e8b807819cd5aec0fa59b247b34d608dc392b
Xander Flood <xander.t.flood@gmail.com>
2020-04-07 20:53:35 -0400

    finalize the hook

--- hooks.tf
+++ hooks.tf
@@ -1,14 +1,6 @@
-# comma-separated lists of client_ids to map into different roles
-resource "auth0_rule" "user" {
-  name   = "ataper"
-  script = file("js/user.js")
-
-  enabled = true
-}
-
 resource "auth0_hook" "internal" {
-  name = "InternalClient Credential Exchange Hook"
-  script = templatefile("js/internal.js", {
+  name = "Client Credential Exchange"
+  script = templatefile("js/credentials-exchange.js", {
     anonymous_client_id = auth0_client.anonymous.client_id
     rules_client_id     = auth0_client.rules.client_id
   })
