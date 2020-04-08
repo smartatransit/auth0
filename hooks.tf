24beb89828997ad80fe3e9386d8dd80a5e5bb586
Xander Flood <xander.t.flood@gmail.com>
2020-04-07 21:44:38 -0400

    fix spurious diffs

--- hooks.tf
+++ hooks.tf
@@ -1,5 +1,5 @@
 resource "auth0_hook" "internal" {
-  name = "Client Credential Exchange"
+  name = "client-credential-exchange"
   script = templatefile("js/credentials-exchange.js", {
     anonymous_client_id = auth0_client.anonymous.client_id
     rules_client_id     = auth0_client.rules.client_id
