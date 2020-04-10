4e1afd1bec4f6ef9fc77590152c8b83a6e04c7ef
Xander Flood <xander.t.flood@gmail.com>
2020-04-10 16:01:25 -0400

    change outputs

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,9 @@
+output "api_url" {
+  value = "https://${var.auth0_domain}"
+}
+output "audience" {
+  value = local.ataper_audience
+}
 output "anonymous_client" {
   value = auth0_client.anonymous
-}
-output "ataper_api_public_keys_uri" {
-  value = "https://${var.auth0_domain}/.well-known/jwks.json"
 }
