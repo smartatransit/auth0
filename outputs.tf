713885c1b2456692c9505579ca8921e08c8e7f53
Xander Flood <xander.t.flood@gmail.com>
2020-09-18 20:25:42 -0400

    add developer access key for testing

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,6 @@ output "audience" {
 output "anonymous_client" {
   value = auth0_client.anonymous
 }
+output "developer_access_key" {
+  value = "${auth0_client.developer.id}|${auth0_client.developer.secret}"
+}
